4b346be8a5714501e1f80c57aed04202a739d100
Emlyn Matheson <emlyn@emlyn.xyz>
2025-03-25 17:38:05 +1100

    Student, Person, Staff classes

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,17 @@
+public class Student extends Person {
+    private String email;
+    private String studentID;
+
+    private Submission[] submissions;
+
+    public void sendEmailMsg(String) {
+        // todo: unimplemented
+    }
+
+    public float totalGrade() {
+        // todo: unimplemented (the design model doesnt include a single grade field ever)
+
+        return 98.5f;
+
+    }
+}
